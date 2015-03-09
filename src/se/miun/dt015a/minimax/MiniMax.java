db4014ebaee0ab1ec9323224131977113adb6449
Sasamus <labus93@gmail.com>
2015-03-09 13:23:52 +0100

    Started implementing the algorithm

--- src/se/miun/dt015a/minimax/MiniMax.java
+++ src/se/miun/dt015a/minimax/MiniMax.java
@@ -1,13 +1,42 @@
 package se.miun.dt015a.minimax;
 
+import java.util.ArrayList;
+
 /**
  * Implement the minimax algorithm with alpha-beta pruning.
+ * 
  * @author Christoffer Fink
  */
-public class MiniMax<State,Action> {
+public class MiniMax<State, Action> {
 
-  public Action getAction(Game<State, Action> game, State state) {
-    return null; // Your code here.
-  }
+	Game game;
+	
+	public Action getAction(Game<State, Action> game, State state) {
+		this.game = game;
+		
+		ArrayList<Successor<State, Action>> successors = (ArrayList<Successor<State, Action>>) game.getSuccessors(state);
+		
+		for(Successor<State, Action> successor : successors){
+			
+			getMaxValue(successor.state);
+		}
+		
+		
+		return null; // Your code here.
+	}
+
+	State getMaxValue(State state) {
+		
+		if(game.isTerminal(state)){
+			return state;
+		}
+		
+		ArrayList<Successor<State, Action>> successors = (ArrayList<Successor<State, Action>>) game.getSuccessors(state);
+		
+		for(Successor<State, Action> successor : successors){
+			
+			getMinValue(successor.state);
+		}
+	}
 
 }
